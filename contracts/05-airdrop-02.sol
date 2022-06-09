35bfd0292891f410ebf2029e72e22e81604a6b6a
RichSilva <richsilva07@outlook.com>
2022-06-09 18:04:13 -0300

    Implementação do contrato 05-airdrop-02

---
+++ contracts/05-airdrop-02.sol
@@ -0,0 +1,101 @@
+//SPDX-License-Identifier: GPL-3-0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./03-token.sol";
+
+contract Airdrop {
+
+    // Using Libs
+
+    // Struct
+
+    // Enums
+    enum status { ACTIVE, PAUSED, CANCELLED } // ACTIVE=0, PAUSED=1, CANCELLED=2 -- é como se fosse um uint8
+
+    // Properties
+    address private owner;
+    address public tokenAddress;
+    address[] private subscribers;
+    status contractState;
+
+    // Modifiers
+    modifier isOwner() {
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
+    }
+
+    // Events
+
+    // Constructor
+    constructor(address token) {
+        owner = msg.sender;
+        tokenAddress = token; // endereço do contrato 03-token que tem o cryptotoken
+        contractState = status.ACTIVE;
+
+    }
+
+    // Public Functions
+    function subscribe() public returns(bool) {
+        require(contractState == status.ACTIVE, "The contract is not ACTIVE!");
+        
+        address newSubscriber = msg.sender;
+
+        hasSubscribed(newSubscriber);
+        subscribers.push(newSubscriber);
+
+        return true;
+    }
+
+    function execute() public isOwner returns(bool) {
+        
+        uint256 balance = CryptoToken(tokenAddress).balanceOf(address(this));
+        require(balance > 0, "Insufficient balance to start airdrop");
+
+        uint256 amountToTranfer = balance / subscribers.length;
+        for (uint256 i = 0; i < subscribers.length; i++) {
+            require(subscribers[i] != address(0));
+            require(CryptoToken(tokenAddress).transfer(subscribers[i], amountToTranfer));
+        }
+
+        return true;
+    }
+
+    function state() public view returns(status) {
+        return contractState;
+    }
+
+    function changeState(uint8 newState) public isOwner returns(status) {
+
+        require(newState < 3, "Invalid status option!");
+
+        if (newState == 0) {
+            require(contractState != status.ACTIVE, "The status is already ACTIVE");
+            contractState = status.ACTIVE;
+        } else if (newState == 1) {
+            require(contractState != status.PAUSED, "The status is already PAUSED");
+            contractState = status.PAUSED;
+        } else {
+            require(contractState != status.CANCELLED, "The status is already CANCELLED");
+            contractState = status.CANCELLED;
+        }
+
+        return contractState;
+    }
+
+    // Private Functions
+    function hasSubscribed(address subscriber) private view returns(bool) {
+        for (uint256 i = 0; i < subscribers.length; i++) {
+            require(subscriber != subscribers[i], "This wallet is already subscribed!");
+        }
+
+        return false;
+    }
+
+    // Kill
+    function kill() public isOwner {
+        require(contractState == status.CANCELLED, "It's necessary to cancel the contract before to kill it!");
+        selfdestruct(payable(owner));
+    }
+
+}
